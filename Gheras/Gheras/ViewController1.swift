b554fc1c8886a5f5529538e2f318f6af172c65f2
Ahmad MacBook <ahmad@Ahmads-MacBook-Pro.local>
2021-12-15 12:38:52 +0300

    now we start

---
+++ Gheras/Gheras/ViewController1.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gheras
+//
+//  Created by Ahmad MacBook on 15/12/2021.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
