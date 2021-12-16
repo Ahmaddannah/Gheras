35d04ad5232a8eb53a4d0803dbdd424310fac895
Aldanah <Aldanah.Saud.ruh.s@tuwaiq.edu.sa>
2021-12-16 10:30:48 +0300

    Reaching User Location - Aldanah

--- Gheras1/Gheras/MapsVC.swift
+++ Gheras1/Gheras/MapsVC.swift
@@ -8,15 +8,16 @@
 import UIKit
 import Firebase
 import GoogleMaps
-import CoreLocation
+//import CoreLocation
 
 
-class MapsVC: UIViewController, CLLocationManagerDelegate {
+class MapsVC: UIViewController , CLLocationManagerDelegate , GMSMapViewDelegate {
     
     
-    let myLocation = CLLocationManager()
+    var myLocation = CLLocationManager()
     
-    let mapView: GMSMapView = GMSMapView(frame: CGRect(x: 0, y: 0, width: 500, height: 900))
+    var mapView: GMSMapView! = GMSMapView(frame: CGRect(x: 0, y: 0, width: 500, height: 900))
+    var coordinatesAddressLabel = UILabel()
 //    UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 900)) as! GMSMapView
     var pin = UIImageView()
     let newBtn = UIButton()
@@ -26,14 +27,27 @@ class MapsVC: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         view.backgroundColor = .orange
         
+        
         // Location
         myLocation.delegate = self
         myLocation.requestWhenInUseAuthorization()
         myLocation.startUpdatingLocation()
-
+        
+        
         //MapView
-//        mapView.backgroundColor = .orange
-//        mapView.frame = CGRect(x: 0, y: 0, width: 500, height: 900)
+        //initializeMapView
+        // myLocationButton: Enables or disables the My Location button
+        // isMyLocationEnabled: Enabling Location blue dot
+        mapView.delegate = self
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
+
+        
+        //Label
+        coordinatesAddressLabel = UILabel(frame: CGRect(x: 20, y: 730, width: 400, height: 80))
+        coordinatesAddressLabel.textColor = .purple
+        coordinatesAddressLabel.backgroundColor = .white
+        
 
         
         //Pin
@@ -51,9 +65,91 @@ class MapsVC: UIViewController, CLLocationManagerDelegate {
         //Add Subviews
         view.addSubview(mapView)
         view.addSubview(pin)
+        view.addSubview(coordinatesAddressLabel)
 //        view.addSubview(newBtn)
         
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 17.0)
+            
+            self.mapView.animate(to: camera)
+            self.myLocation.stopUpdatingLocation()
+
+            
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    // MARK: - Activate my location button
+       // initiate action for my location button
+       func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
+           return false
+       }
+    
+    // MARK: - Add Markers
+    //   long press on the map to add marker
+    func mapView(_ mapView: GMSMapView, didLongPressAt coordinate: CLLocationCoordinate2D) {
         
+        // initiate  marker
+        let marker = GMSMarker()
+        
+        // setup marker position
+        marker.position = coordinate
+        
+        // getting marker coordinates
+        let markerLatitude = Float( coordinate.latitude )
+        let markerLongitude = Float(coordinate.longitude)
+        
+        
+        marker.title = "الإحداثيات"
+        marker.snippet = "\(markerLatitude)\n\(markerLongitude)"
+        marker.map = mapView
+    }
+    
+   
+    
+    
+    // MARK: - Getting Readable Address
+
+    // detecting camera position whenever the position changed
+    func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+        let latitude = mapView.camera.target.latitude
+        let longitude = mapView.camera.target.longitude
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        // take coordinates as parameter to find string output
+        reverseGeocodeLocation(coordinate: location)
+        
+    }
+    
+    func reverseGeocodeLocation(coordinate: CLLocationCoordinate2D) {
+        
+        //1 - Creating and intiating GMSGeocoder Object to be
+        
+        let geocoder = GMSGeocoder()
+        //2 - use geocoder to reverse geocode the coordinates
+        
+        geocoder.reverseGeocodeCoordinate(coordinate) { response, error in
+            
+            if let address = response?.firstResult(),
+                let lines = address.lines {
+                
+                //3 - setting coordinatesAddressLabel text to current coordinates address
+                
+                print(lines.joined(separator: "\n") )
+                self.coordinatesAddressLabel.text = lines.joined(separator: "\n")
+            }
+        }
+    }
+    
+    // MARK: - Label Design
+    func settingLabelDesign(){
+        coordinatesAddressLabel.layer.masksToBounds = true
+        coordinatesAddressLabel.layer.cornerRadius = 30
     }
 
 }
